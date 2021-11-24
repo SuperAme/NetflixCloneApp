07adc2ab4c846fa12f8b23c53f2abbbe81c6d5ba
AmericoMeneses <americo.meneses@globant.com>
2021-11-24 09:41:43 -0600

    first commit

---
+++ GNetTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GNetTest
+//
+//  Created by Americo Meneses Quintero on 24/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
